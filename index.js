3362629d271c87fefa7bd75da14ea837186c7103
Ryan Riley Puzon <rrgpuzon@gmail.com>
2022-02-22 22:51:50 -0800

    sets up the basic socket server up

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const { Server } = require('socket.io')
+const express = require('express');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server);
+
+app.get('/', (req, res, next) => {
+	res.sendFile(__dirname + "/index.html");
+})
+
+io.on('connection', (socket) => {
+	console.dir(socket);
+	console.log('a user connection');
+})
+
+const {PORT} = process.env;
+server.listen(PORT, () => {
+	console.log(`listening on http://localhost:${PORT}`)
+})
